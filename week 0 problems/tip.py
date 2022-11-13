df4542b53ee5470ba2816db3295fc9625a64a454
Ivan Matias <ivanmatias1331@gmail.com>
2022-11-13 00:04:55 -0800

    finished tip problem

--- week 0 problems/tip.py
+++ week 0 problems/tip.py
@@ -1,5 +1,16 @@
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
-    percent = percent_to_float(input("What percentage would you like to tip?"))
+    percent = percent_to_float(input("What percentage would you like to tip?" ))
     tip = dollars * percent
     print(f"Leave ${tip:.2f}")
+
+def dollars_to_float(d):
+    #Changing the formatting from $##.## to a float
+    return(float(d.replace("$","")))
+
+def percent_to_float(p):
+    #Changing the formatting from ##% to a float
+    percentage = (float(p.replace("%","")))
+    return(percentage/100)
+
+main()
