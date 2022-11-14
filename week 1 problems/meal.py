547d90ac61455a74b70fa84c5a06a55a8475a74f
Ivan Matias <ivanmatias1331@gmail.com>
2022-11-13 23:45:57 -0800

    finished problem 5 for week 1

--- week 1 problems/meal.py
+++ week 1 problems/meal.py
@@ -15,12 +15,25 @@ Assume the users input is in 24 hour time as
 Assume that the meal's time range is inclusive
     So it's either 7:00, 8:00, or in between any of those times
 """
+#Approach: Convert hours into minutes and add with minutes
+#Then we can check if it's in between two minute values rather than hours and minutes
 
 def main():
     time = input("What time is it? ")
+    minutes = convert(time)
+    #Breakfast time (7am and 8am)
+    if minutes >= 420 and minutes <= 480:
+        print("breakfast time")
+    elif minutes >= 720 and minutes <= 780:
+        print("lunch time")
+    elif minutes >= 1080 and minutes <= 1140:
+        print("dinner time")
     
 
 def convert(time):
+    hrs, min = time.split(":")
+    #Multiply hours by 60 min and adding with minutes
+    return((int(hrs) * 60) + int(min))
 
 if __name__ == "__main__":
     main()
