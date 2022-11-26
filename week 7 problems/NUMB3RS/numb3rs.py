9c6a4cd15e3b417c746b6f88c28390c313998a11
Ivan Matias <ivanmatias1331@gmail.com>
2022-11-26 04:54:34 -0800

    Added comments to problem 1

--- week 7 problems/NUMB3RS/numb3rs.py
+++ week 7 problems/NUMB3RS/numb3rs.py
@@ -9,21 +9,25 @@ Output a string that returns True or False
 """
 
 import re
-import sys
 
 def main():
     print(validate(input("IPv4 Address: ")))
     
 def validate(ip):
+    #Check if the input is correct through regular expressions
     test = re.search(r"([1-9]+)\.([1-9]+)\.([1-9]+)\.([1-9]+)", ip)
+    #If the input passes the regular expressions check
     if test:
+        #For every group (...) in the regular expression
         for group in test.groups():
             try:
+                #Try to convert the current group into an integer
                 current_num = int(group)
             except ValueError:
+                #If it doesn't work, return False
                 print("One of these are not a number")
                 return(False)
-                
+            #If the current group is an integer, check if it's between 0 and 225
             if current_num < 0 or current_num > 225:
                 print("One of these numbers don't fit in the range of 225")
                 return(False)
