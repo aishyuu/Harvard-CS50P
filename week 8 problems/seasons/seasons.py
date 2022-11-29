9b60ddb57b9bfaa9979fee867261945a647812cc
Ivan Matias <ivanmatias1331@gmail.com>
2022-11-29 11:11:00 -0800

    Started problems for week 8

---
+++ week 8 problems/seasons/seasons.py
@@ -0,0 +1,15 @@
+"""
+Implement a program that prompts the user for their date of birth
+    YYYY-MM-DD
+
+Format and print how old they are in minutes.
+    Round to the nearest integer
+    
+Assume for simplicity, the user was born at midnight
+
+Use datetime.date.today to get today's date
+"""
+
+import datetime
+
+print(datetime.date.today())
